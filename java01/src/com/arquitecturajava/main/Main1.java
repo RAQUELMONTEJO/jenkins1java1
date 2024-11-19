c9197e83bbb4544354c3724daf31f416837db5b6
raque <raque@Raquel>
2024-11-19 12:54:59 +0100

    primera versión de pruebas

---
+++ java01/src/com/arquitecturajava/main/Main1.java
@@ -0,0 +1,11 @@
+package com.arquitecturajava.main;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// Clase de prueba para utilizar con Jenkins
+		System.out.println("hola");
+
+	}
+
+}
